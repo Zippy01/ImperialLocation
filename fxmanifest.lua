d665a4b4062c0fa1e9526d2b90f776ac98024923
Zippy OFC <72061397+Zippy01@users.noreply.github.com>
2025-02-19 03:04:54 -0500

    update description for Imperial Location integration

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Zippy OFC'
-description 'ImperialCAD API Integration Library'
+description 'Imperial Location integration'
 version '1.0'
 
 server_script 'main.lua'
